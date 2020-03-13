d53b21f0696d1d66a52c75afa68f81d75cfb7e19
mafshin <mafshin89@gmail.com>
2020-03-13 20:29:50 +0330

    SqlScripts

---
+++ ExaScale/PerformanceTest/SqlScript/Customers-Main.sql
@@ -0,0 +1,95 @@
+﻿/****** Object:  Table [dbo].[ShardKeyMap]    Script Date: 3/13/2020 8:25:56 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ShardKeyMap](
+	[ShardKey] [nvarchar](100) NULL,
+	[ShardId] [int] NULL,
+ CONSTRAINT [IX_ShardKeyMap] UNIQUE NONCLUSTERED 
+(
+	[ShardId] ASC,
+	[ShardKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Shards]    Script Date: 3/13/2020 8:25:56 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Shards](
+	[ShardId] [int] NOT NULL,
+ CONSTRAINT [PK_Shards] PRIMARY KEY CLUSTERED 
+(
+	[ShardId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ShardKeyMap]  WITH CHECK ADD  CONSTRAINT [FK_ShardKeyMap_Shards] FOREIGN KEY([ShardId])
+REFERENCES [dbo].[Shards] ([ShardId])
+GO
+ALTER TABLE [dbo].[ShardKeyMap] CHECK CONSTRAINT [FK_ShardKeyMap_Shards]
+GO
+/****** Object:  StoredProcedure [dbo].[AddShard]    Script Date: 3/13/2020 8:25:56 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[AddShard]
+@shardId int
+AS
+BEGIN
+insert into Shards
+(ShardId)
+VALUES
+(@shardId)
+END
+GO
+/****** Object:  StoredProcedure [dbo].[AddShardKey]    Script Date: 3/13/2020 8:25:56 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[AddShardKey]
+@shardId int,
+@shardKey nvarchar(100)
+AS
+BEGIN
+INSERT INTO dbo.ShardKeyMap
+(ShardKey, ShardId)
+VALUES
+(@shardKey, @shardId)
+END
+GO
+/****** Object:  StoredProcedure [dbo].[ClearAll]    Script Date: 3/13/2020 8:25:56 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE proc [dbo].[ClearAll]
+AS
+Truncate Table ShardKeyMap
+DELETE FROM Shards
+GO
+/****** Object:  StoredProcedure [dbo].[GetShardMap]    Script Date: 3/13/2020 8:25:56 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [dbo].[GetShardMap]
+as
+begin
+select * from ShardKeyMap
+SELECT * FROM shards
+END
+GO
+/****** Object:  StoredProcedure [dbo].[GetShards]    Script Date: 3/13/2020 8:25:56 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [dbo].[GetShards]
+AS
+SELECT * FROM dbo.Shards
+GO
---
+++ ExaScale/PerformanceTest/SqlScript/Customers-Shard.sql
@@ -0,0 +1,99 @@
+﻿/**** Object:  Table [dbo].[Customers]    Script Date: 3/13/2020 8:28:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customers](
+	[CustomerId] [int] NOT NULL,
+	[Title] [nvarchar](100) NOT NULL,
+	[Birthdate] [date] NOT NULL,
+	[Address] [nvarchar](100) NULL,
+	[Job] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[CustomerId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Orders]    Script Date: 3/13/2020 8:28:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Orders](
+	[OrderId] [bigint] IDENTITY(1,1) NOT NULL,
+	[CustomerId] [int] NOT NULL,
+	[Count] [int] NOT NULL,
+	[Price] [int] NOT NULL,
+	[Description] [nvarchar](1000) NOT NULL,
+	[OrderEntryDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+(
+	[OrderId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Customers] FOREIGN KEY([CustomerId])
+REFERENCES [dbo].[Customers] ([CustomerId])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Customers]
+GO
+/****** Object:  StoredProcedure [dbo].[AddCustomer]    Script Date: 3/13/2020 8:28:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE proc [dbo].[AddCustomer]
+@customerId int,
+@title nvarchar(100),
+@birthdate date,
+@address nvarchar(100),
+@job nvarchar(100)
+AS
+BEGIN
+insert into Customers
+(CustomerId, Title, Birthdate, Address, Job)
+VALUES
+(@customerId, @title, @birthdate, @address, @job)
+END
+GO
+/****** Object:  StoredProcedure [dbo].[AddOrder]    Script Date: 3/13/2020 8:28:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+  create proc [dbo].[AddOrder]
+  @customerId int,
+  @count int,
+  @price decimal(18, 0),
+  @description nvarchar(1000),
+  @orderEntryDate datetime
+  AS
+  BEGIn
+
+  insert into Orders
+  VALUES
+  (@customerId, @count, @price, @description, @orderEntryDate)
+  END
+GO
+/****** Object:  StoredProcedure [dbo].[ClearAll]    Script Date: 3/13/2020 8:28:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[ClearAll]
+AS
+DELETE FROM Orders
+DELETE FROM Customers
+GO
+/****** Object:  StoredProcedure [dbo].[GetShardItemsCount]    Script Date: 3/13/2020 8:28:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[GetShardItemsCount]
+AS
+BEGIn
+Select count(1) as Count from Customers
+END
+GO
